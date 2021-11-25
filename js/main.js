921ceee5faa5ab4c8818d28e86a35de4396070c6
Srujan Patel <srujan.patel@cern.ch>
2021-11-25 14:04:12 +0100

    Add education section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
+const edu = document.querySelector('#education')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
+const eduContent = document.querySelector('#edu-content')
 
 about.addEventListener('click', () => {
     const aboutBox = new WinBox({
         title: 'About Me',
         width: '400px',
-        // modal: true,
         height: '400px',
         top: 20,
         right: 20,
@@ -33,6 +34,26 @@ contact.addEventListener('click', () => {
         bottom: 0,
         left: 125,
         mount: contactContent,
+        onfocus: function () {
+            this.setBackground('#00aa00')
+        },
+        onblur: function () {
+            this.setBackground('#777')
+        }
+    })
+})
+
+edu.addEventListener('click', () => {
+    const eduBox = new WinBox({
+        title: 'Education',
+        modal: true,
+        width: '400px',
+        height: '400px',
+        top: 60,
+        right: 35,
+        bottom: 0,
+        left: 135,
+        mount: eduContent,
         onfocus: function () {
             this.setBackground('#00aa00')
         },
